8090c0bfccecbb4bafd3e20a021d60394cb5407a
yzrsamet <yzr.samet@gmail.com>
2020-12-26 23:37:44 +0100

    Import Entity in IPersonService Remove Generic Person extension from IPersonService

--- PersonBook/PersonBook/PersonBook/Service/IPersonService.cs
+++ PersonBook/PersonBook/PersonBook/Service/IPersonService.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace PersonBook
 {
-    interface IPersonService<Person>
+    using Entity;
+    interface IPersonService
     {
         ICollection<Person> ListPersons();
         void DeletePerson(int id);
